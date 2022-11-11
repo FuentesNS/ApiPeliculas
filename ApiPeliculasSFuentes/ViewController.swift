5a6a09d7ee477d96063605396a894ea97a9f2422
Samuel <fnsamuel31@gmail.com>
2022-11-11 10:29:47 -0600

    Initial Commit

---
+++ ApiPeliculasSFuentes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiPeliculasSFuentes
+//
+//  Created by MacBookMBA1 on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
